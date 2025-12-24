b40edb7a14e23622526ff75269fa50796723ab3c
sohee52 <wneh2023@naver.com>
2025-12-24 12:42:06 +0900

    0016

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -6,11 +6,13 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
 @MappedSuperclass // JPA Entity 들이 BaseEntity 를 상속할 경우, BaseEntity 의 필드들도 컬럼으로 인식하게 함
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 // 모든 엔티티들의 조상
-public class BaseEntity {
+public abstract class BaseEntity {
     public String getModelTypeCode() {
         return this.getClass().getSimpleName();
     }
@@ -18,4 +20,8 @@ public class BaseEntity {
     protected void publishEvent(Object event) {
         GlobalConfig.getEventPublisher().publish(event);
     }
+
+    public abstract int getId();
+    public abstract LocalDateTime getCreateDate();
+    public abstract LocalDateTime getModifyDate();
 }
