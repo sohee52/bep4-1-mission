477aed14f2bfad9c8e9098b0f4c0eb8f252a5410
sohee52 <wneh2023@naver.com>
2025-12-24 12:17:48 +0900

    0014

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -1,7 +1,7 @@
 package com.back.boundedContext.post.app;
 
-import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.out.MemberApiClient;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-    private final MemberFacade memberFacade;
+    private final MemberApiClient memberApiClient;
 
     public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
@@ -27,7 +27,9 @@ public class PostWriteUseCase {
                 )
         );
 
-        String randomSecureTip = memberFacade.getRandomSecureTip();
+        // API 호출 (동기, 응답 필요함)
+        // → Member 모듈에서 보안 팁 문자열을 받아옴
+        String randomSecureTip = memberApiClient.getRandomSecureTip();
 
         return new RsData<>("201-1", "%d번 글이 생성되었습니다. 보안 팁 : %s".formatted(post.getId(), randomSecureTip), post);
     }
