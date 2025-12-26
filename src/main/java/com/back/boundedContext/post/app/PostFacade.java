445363779ee1712bab1ec1d7132b34b7ff61244d
sohee52 <wneh2023@naver.com>
2025-12-26 15:39:57 +0900

    0028

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -1,10 +1,7 @@
 package com.back.boundedContext.post.app;
 
-import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.domain.PostMember;
-import com.back.boundedContext.post.out.PostMemberRepository;
-import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.rsData.RsData;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +13,13 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor // final 이 붙은 필드에 대해 생성자를 자동으로 생성
 public class PostFacade {
-    private final PostRepository postRepository;
+    private final PostSupport postSupport;
+    private final PostSyncMemberUseCase postSyncMemberUseCase;
     private final PostWriteUseCase postWriteUseCase;
-    private final PostMemberRepository postMemberRepository;
 
     @Transactional(readOnly = true)
     public long count() {
-        return postRepository.count();
+        return postSupport.count();
     }
 
     @Transactional
@@ -32,26 +29,16 @@ public class PostFacade {
 
     @Transactional(readOnly = true)
     public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+        return postSupport.findById(id);
     }
 
     @Transactional
     public PostMember syncMember(MemberDto member) {
-        PostMember postMember = new PostMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
-        );
-
-        return postMemberRepository.save(postMember);
+        return postSyncMemberUseCase.syncMember(member);
     }
 
     @Transactional(readOnly = true)
     public Optional<PostMember> findPostMemberByUsername(String username) {
-        return postMemberRepository.findByUsername(username);
+        return postSupport.findPostMemberByUsername(username);
     }
 }
